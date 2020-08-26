1b78132728b3886c766cf4e7bdac3569472005d9
Michael <michaelsoliman981@gmail.com>
2020-08-26 16:58:33 +0200

    added side drawer and orders screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './screens/orders_screen.dart';
 import './screens/cart_screen.dart';
+
 import './providers/products.dart';
 import './providers/orders.dart';
 import './providers/cart.dart';
@@ -36,6 +38,7 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
         },
       ),
     );
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -6,6 +6,7 @@ import '../screens/cart_screen.dart';
 import '../providers/cart.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/app_drawer.dart';
 import '../widgets/badge.dart';
 
 enum Filters {
@@ -65,6 +66,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showFavs),
     );
   }
